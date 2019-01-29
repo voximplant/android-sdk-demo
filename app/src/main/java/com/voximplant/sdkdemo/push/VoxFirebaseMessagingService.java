de2e81b19ed92e13b1e78f0770cda437b2980847
YuliaGrigorieva <miss000hara@gmail.com>
2019-01-29 13:51:50 +0300

    Update gradle plugin to the latest and Voximplant Android SDK to 2.9.0

--- app/src/main/java/com/voximplant/sdkdemo/push/VoxFirebaseMessagingService.java
+++ app/src/main/java/com/voximplant/sdkdemo/push/VoxFirebaseMessagingService.java
@@ -6,10 +6,7 @@ package com.voximplant.sdkdemo.push;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-import com.voximplant.sdk.Voximplant;
-import com.voximplant.sdk.messaging.IMessengerEvent;
 import com.voximplant.sdkdemo.Shared;
-import com.voximplant.sdkdemo.utils.NotificationHelper;
 
 import java.util.Map;
 
@@ -21,11 +18,6 @@ public class VoxFirebaseMessagingService extends FirebaseMessagingService {
 
         if (push.containsKey("voximplant")) {
             Shared.getInstance().getClientManager().pushNotificationReceived(remoteMessage.getData());
-        } else if (push.containsKey("voximplant_im")){
-            IMessengerEvent event = Voximplant.getMessengerPushNotificationProcessing().processPushNotification(push);
-            if (event != null) {
-                NotificationHelper.get().buildMessengerNotification(event, getApplicationContext());
-            }
         }
     }
 
